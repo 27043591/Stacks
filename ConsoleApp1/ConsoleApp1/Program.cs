a378c80527f407bdc3785598717ae92aea11b8c4
Maheshwar Maheshwar <27043591@student.toiohomai.ac.nz>
2019-03-18 16:39:45 +1300

    strick

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".......stack......");
+
+            Stack s1 = new Stack();
+
+            string contains = "";
+            Console.WriteLine("first number ");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("second number");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("third number");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("forth number");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("fivth number");
+            s1.Push(Console.ReadLine());
+           
+           
+
+            Console.WriteLine("....contains....");
+            Console.WriteLine("number in the stack");
+            contains = Console.ReadLine();
+            if (s1.Contains(contains) == true)
+            {
+                while (s1.Peek().ToString() != contains)
+                {
+                    Console.WriteLine(s1.Pop()); 
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("doest not contains" + contains);
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".......stack......");
+
+            Stack s1 = new Stack();
+
+          
+            Console.WriteLine("first number ");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("second number");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("third number");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("forth number");
+            s1.Push(Console.ReadLine());
+            Console.WriteLine("fivth number");
+            s1.Push(Console.ReadLine());
+
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+            Console.WriteLine(s1.Pop());
+
+            Console.ReadLine();
+        }
+        
+    }
+}
